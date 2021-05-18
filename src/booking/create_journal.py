539a0150ba47d82f4e809e633e9f2c20f8ea62cc
sapeint/chanku <sapeint@hashbang.sh>
2021-05-17 21:37:02 -0500

    Cleanup create_journal.py

--- src/booking/create_journal.py
+++ src/booking/create_journal.py
@@ -19,37 +19,25 @@ class Journal:
             f.write("Page 1\n")
 
 
-def verified_input(prompt: Any, verifier: Callable[[str], bool]) -> str:
-    """ Checks and ensures input is correct, and
-    reprompts user if it is incorrect. """
-    while not verifier((i := input(prompt))):
-        pass
-    return i
-
-
 def default_input(prompt: Any, default: str) -> str:
-    """ This gets a user input, and returns the default if
-    nothing is input."""
-    return (verified_input(prompt, lambda: True) or default)
+    """ It's basically input, but with a default option
+    if the entered value is blank. """
+    return (input(prompt) or default)
 
 
 if __name__ == "__main__":
     name = default_input(("Please enter the name of the Journal"
                           "[Default: General]: "),
-                         lambda x: bool(x) or not boo,
                          "General")
     abbr = default_input(("Please enter the abbreviation for the journal"
                           "[Default: {}]: "
                           ).format(name[:3].upper()),
-                         lambda x: bool(x),
                          name[:3].upper())
     filename = default_input(("Please input the filename for the journal"
                               "[Default: {}.jnl]: "
                               ).format(name.lower()),
-                             lambda x: bool(x),
                              "{}.jnl".format(name.lower()))
     path = default_input("Please input the path for the file to be made at"
                          "[Default: ./]: ",
-                         lambda x: bool(x),
                          "./")
     Journal(name=name, abbreviation=abbr).create_journal(filename, path)
